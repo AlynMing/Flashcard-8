fef25687b6539c7cc79212f401b3f2156b5e1445
natemek <natnaelmek@gmail.com>
2020-02-15 14:53:49 -0500

    Initial Commit

---
+++ Flashcard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcard
+//
+//  Created by Natnael Mekonnen on 2/15/20.
+//  Copyright © 2020 natemek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
